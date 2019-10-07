5e007b6d09899f50c1565f40185b69a43da8d398
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-06 19:43:55 -0700

    new customer method

--- orders/src/main/java/com/krishannattar/orders/controllers/CustomerController.java
+++ orders/src/main/java/com/krishannattar/orders/controllers/CustomerController.java
@@ -3,11 +3,15 @@ package com.krishannattar.orders.controllers;
 import com.krishannattar.orders.models.Customer;
 import com.krishannattar.orders.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -25,9 +29,28 @@ public class CustomerController {
         List<Customer> myCustomers = customerService.findAll();
         return new ResponseEntity<>(myCustomers, HttpStatus.OK);
     }
-    
+
     //http://localhost:2019/customer/name/{custname}
+    @GetMapping(value = "/customer/name/{custname}",
+    produces = {"application/json"})
+    public ResponseEntity<?> findCustomerByName(@PathVariable String custname)
+    {
+        Customer returnCustomer = customerService.findCustomerByName(custname);
+        return new ResponseEntity<>(returnCustomer, HttpStatus.OK);
+    }
+
+
     //http://localhost:2019/data/customer/new
+    @PostMapping(value = "/data/customer/new",
+    produces = {"application/json"})
+    public ResponseEntity<?> addNewCustomer(@Valid @RequestBody Customer newCustomer) throws URISyntaxException
+    {
+        newCustomer = customerService.save(newCustomer);
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newCustomerURI = ServletUriComponentsBuilder.fromCurrentRequest().path("/{custname}").buildAndExpand(newCustomer.getCustname()).toUri();
+        responseHeaders.setLocation(newCustomerURI);
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+    }
     //http://localhost:2019/data/customer/update/{custcode}
     //http://localhost:2019/data/customer/delete/{custcode}
 
