313fb2c420844fac220ff3036125860510639975
Krishan Nattar <krishan.nattar@gmail.com>
2019-10-06 22:40:24 -0700

    delete agent endpoint working

--- orders/src/main/java/com/krishannattar/orders/controllers/AgentController.java
+++ orders/src/main/java/com/krishannattar/orders/controllers/AgentController.java
@@ -1,4 +1,27 @@
 package com.krishannattar.orders.controllers;
 
+import com.krishannattar.orders.models.Agent;
+import com.krishannattar.orders.services.AgentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AgentController {
+
+    @Autowired
+    private AgentService agentService;
+
+    //http://localhost:2019/agent/{agentcode}
+    @DeleteMapping(value = "/agent/{agentcode}",
+            produces = {"application/json"})
+    public ResponseEntity<?> deleteAgent(@PathVariable long agentcode) {
+//        Agent currentAgent = new Agent();
+
+        agentService.delete(agentcode);
+        return new ResponseEntity<>("Delete Successful", HttpStatus.OK);
+    }
 }
